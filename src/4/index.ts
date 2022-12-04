e176a834ce71d4be22625daeec0cf4a2b3b48505
Vicky Thrasher <vic07kyt@gmail.com>
2022-12-03 21:42:40 -0800

    Day 4 pt 1

---
+++ src/4/index.ts
@@ -0,0 +1,30 @@
+function solve() {
+  const fileio = require('/Users/vicky/Documents/Projects/AdventOfCode2022/src/common/fileio');
+
+  let input = fileio.readLines('src/4/input.txt');
+  input.pop();
+  console.log(input);
+
+  let containCount = 0;
+
+  input.forEach(pairing => {
+    let pair = pairing.split(',');
+    let elf1 = pair[0].split('-');
+    let elf2 = pair[1].split('-');
+
+    let elf1Min = parseInt(elf1[0]);
+    let elf1Max = parseInt(elf1[1]);
+    let elf2Min = parseInt(elf2[0]);
+    let elf2Max = parseInt(elf2[1]);
+
+    if ((elf1Min <= elf2Min && elf1Max >= elf2Max) ||
+        (elf2Min <= elf1Min && elf2Max >= elf1Max)) {
+          containCount++;
+        }
+  });
+
+  console.log(containCount);
+
+}
+
+solve();
